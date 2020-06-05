6443edf22fbaa6f5314bfb87f4769d613c14f8b8
Filipe Vieira <sv.filipe@gmail.com>
2020-06-06 00:39:31 +0100

    update site

---
+++ docs/js/script.js
@@ -0,0 +1,124 @@
+/* global monogatari */
+
+// Define the messages used in the game.
+monogatari.action ('Message').messages ({
+	'Help': {
+		title: 'Help',
+		subtitle: 'Some useful Links',
+		body: `
+			<p><a href='https://monogatari.io/documentation/'>Documentation</a> - Everything you need to know.</p>
+			<p><a href='https://monogatari.io/demo/'>Demo</a> - A simple Demo.</p>
+		`
+	}
+});
+
+// Define the notifications used in the game
+monogatari.action ('Notification').notifications ({
+	'Welcome': {
+		title: 'Welcome',
+		body: 'This is the Monogatari VN Engine',
+		icon: ''
+	}
+});
+
+// Define the Particles JS Configurations used in the game
+monogatari.action ('Particles').particles ({
+
+});
+
+// Define the music used in the game.
+monogatari.assets ('music', {
+
+});
+
+// Define the voice files used in the game.
+monogatari.assets ('voice', {
+
+});
+
+// Define the sounds used in the game.
+monogatari.assets ('sound', {
+
+});
+
+// Define the videos used in the game.
+monogatari.assets ('video', {
+
+});
+
+// Define the images used in the game.
+monogatari.assets ('images', {
+	lobbyBalcony: "balcony.png"
+});
+
+
+// Define the backgrounds for each scene.
+monogatari.assets ('scenes', {
+	lobby: "lobby.png",
+	bossOffice: "boss-office.png"
+});
+	 
+// Colors,
+const colorNavy = "#001f3f";
+const colorBlue = "#0074D9";
+const colorAqua = "#7FDBFF";
+const colorTeal = "#39CCCC";
+const colorOlive = "#3D9970";
+const colorGreen = "#2ECC40";
+const colorLime = "#01FF70";
+const colorYellow = "#FFDC00";
+	 
+
+// Define the Characters
+// Define the Characters
+monogatari.characters ({
+	/*
+	'y': {
+		'Name': 'Yui',
+		'Color': '#5bcaff'
+	}*/
+	"brain": {
+		"Name": "Your Brain!",
+		"Color": colorYellow,
+		"Images":{ // Images Identifier for the "Show" statement.
+			"Normal": "raquel.png",
+		},
+		"Side": {
+			"Normal": "animegirlatbeach.png",
+			"Happy": "animegirlatbeach.png",
+			"Disappointed": "animegirlatbeach.png"
+		}
+	},
+	"you": {
+		"Name": "You",
+		"Color": colorBlue,
+		"Images":{ // Images Identifier for the "Show" statement.
+			"Normal": "animegirlatbeach.png",
+		},
+		"Side": {
+			"Normal": "animegirlatbeach.png",
+			"Happy": "animegirlatbeach.png"
+		}
+	},
+	"raquel": {
+		"Name": "Raquel",
+		"Color": colorAqua,
+		"Images":{ // Images Identifier for the "Show" statement.
+			"Normal": "raquel.png",
+		},
+		"Side": {
+			"Normal": "raquel-face.png",
+			"Happy": "raquel-face.png"
+		}
+	},
+	"boss": {
+		"Name": "Batista",
+		"Color": colorTeal,
+		"Images": {
+			"Normal": "boss/boss.png"
+		},
+		"Side": {
+			"Normal": "boss/boss.png"
+		}
+	}
+});
---
+++ docs/js/story/interview.js
@@ -0,0 +1,40 @@
+"use strict";
+
+monogatari.script({"Interview": [
+	"show scene bossOffice with fadeIn",
+	"show character boss Normal bottom left with lightSpeedIn",
+	"boss:Normal Hi {{player.name}}, before we begin, let me tell you what we do here on 'The Coorporation' ...",
+	"Yeah, finally, the job description only said 'We are hiring, The coorporation.', maybe they had a caracter count limit or something...",
+	"boss:Normal We make money, lots of them, thats what we do and when someone thinks we can't do it, we do it!!!",
+	"brain:Happy Ok great start, now we are going to the bottom of this.",
+	"boss:Normal So what kind of value will you bring to 'The Coorporation'.",
+	"you:Normal Well as you probaly had seen on my resume...",
+	"boss:Normal I don't see resumes, I see people, hard working people that will give their blood and sweat for this company.",
+	{
+		"Choice": {
+			"Dialog": "brain:Normal I don't know what they do, and what I am supposed to do, what should I say?",
+			"TeamPlayer": {
+				"Text": "I am a team player!",
+				"Do": "jump FriendGuy"
+			},
+			"ComputerGuy": {
+				"Text": "I know computers, email sending/reciving, write documents, spreadsheet, powerpoint, charts, piecharts...",
+				"Do": "jump FunnyGuy"
+			},
+			"Hardworker": {
+				"Text": "I am persistent and hard worker, I may not be the best but I always do my best.",
+				"Do": "jump HardworkerGuy"
+			},
+			"AmbitiousGuy": {
+				"Text": "I am the best, if you hire me I will make even more money!",
+				"Do": "jump AmbitiousGuy"
+			},
+			"HonestGuy": {
+				"Text": "I don't understand what the company does, and what would I be doing?",
+				"Do": "jump HonestGuy"
+			}
+		}
+	}
+
+]});
+
